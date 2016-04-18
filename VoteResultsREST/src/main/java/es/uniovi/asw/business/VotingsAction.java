02ef2fdb07efa5b7fa148710bdff7f60b83571a6
Dani <danicuestasuarez@gmail.com>
2016-04-19 01:44:56 +0200

    Added new REST (List votings)

---
+++ VoteResultsREST/src/main/java/es/uniovi/asw/business/VotingsAction.java
@@ -0,0 +1,31 @@
+package es.uniovi.asw.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import es.uniovi.asw.factories.Factories;
+import es.uniovi.asw.model.Voting;
+import es.uniovi.asw.model.VotingMinimalInfoAdapter;
+
+@Service
+public class VotingsAction {
+
+	public VotingsAction(){}
+	@Autowired
+	Factories factories;
+	
+	public List<VotingMinimalInfoAdapter> execute() {
+		List<VotingMinimalInfoAdapter> votings = new ArrayList<>();
+		try {			
+			for (Voting voting : factories.getPersistenceFactory().getVotingRepository().findAll())
+				votings.add(new VotingMinimalInfoAdapter(voting));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return votings;
+	}
+
+}
--- VoteResultsREST/src/main/java/es/uniovi/asw/controller/Main.java
+++ VoteResultsREST/src/main/java/es/uniovi/asw/controller/Main.java
@@ -1,5 +1,7 @@
 package es.uniovi.asw.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import es.uniovi.asw.factories.Factories;
 import es.uniovi.asw.model.Voting;
+import es.uniovi.asw.model.VotingMinimalInfoAdapter;
 import es.uniovi.asw.model.VotingResults;
 
 
@@ -23,9 +26,15 @@ public class Main {
   private static final Logger LOG = LoggerFactory.getLogger(Main.class);
   @Autowired
   Factories factories;
+  
   @RequestMapping(value = "/voting", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE})
   public ResponseEntity<VotingResults> getVotingInfo(@RequestParam("id") long id) {
 	  
   	return  new ResponseEntity<VotingResults>(factories.getBusinessFactory().getResults(id), HttpStatus.OK);
-  	} 
+  	}
+  @RequestMapping(value = "/votings", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE})
+  public ResponseEntity<List<VotingMinimalInfoAdapter>> getVotings() {
+	  
+  	return  new ResponseEntity<List<VotingMinimalInfoAdapter>>(factories.getBusinessFactory().getVotings(), HttpStatus.OK);
+  	}
 }
