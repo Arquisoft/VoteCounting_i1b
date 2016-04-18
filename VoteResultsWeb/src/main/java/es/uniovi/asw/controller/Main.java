c2b3d8a66e7c1ffcc44116dc88ec32792068e8e5
Ovetense <carlos94sm@gmail.com>
2016-04-19 01:31:50 +0200

    Web response, REST and dependencies

--- VoteResultsWeb/src/main/java/es/uniovi/asw/controller/Main.java
+++ VoteResultsWeb/src/main/java/es/uniovi/asw/controller/Main.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -14,9 +15,12 @@ public class Main {
   private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
   @RequestMapping("/")
-  public ModelAndView landing(Model model) {
+  public String landing(Model model) {
     LOG.info("Landing page access");
-    return new ModelAndView("landing");
+    RestTemplate rt = new RestTemplate();
+    String response = rt.getForObject("http://localhost:8080/voting?id={id}", String.class,"1");
+    model.addAttribute("response",response);
+    return "landing";
   }
   
 }
